0351947b3c399c2aae501b45446e53f57dd0f9eb
sarabusaiprashanth <49392113+sarabusaiprashanth@users.noreply.github.com>
2019-04-09 20:32:30 +0530

    Update 3sums.java

--- programs/3sums.java
+++ programs/3sums.java
@@ -1,36 +1,32 @@
 public class Solution {
-    public ArrayList<ArrayList<Integer>> threeSum(ArrayList<Integer> A) {
-                Collections.sort(A);
-        ArrayList<ArrayList<Integer>> ans = new ArrayList<>();
+    public int threeSumClosest(ArrayList<Integer> A, int B) {
         
-        for (int i=0;i<A.size()-2;i++) {
-            if (i > 0 && A.get(i).equals(A.get(i-1))) continue;
-            
-            int target = -A.get(i);
+       Collections.sort(A);
+        int res = 0;
+        int min = Integer.MAX_VALUE;
+        for (int i=0; i< A.size()-2; i++) {
             int j = i+1;
             int k = A.size()-1;
-            
             while (j<k) {
-                if (A.get(j) + A.get(k) == target) {
-                    ArrayList<Integer> temp = new ArrayList<>();
-                    temp.add(A.get(i)); 
-                    temp.add(A.get(j)); 
-                    temp.add(A.get(k));
-                    ans.add(temp);
-                    j++;
-                    k--;
-                    
-                    while(j<k && A.get(j) == A.get(j-1)) j++;
-                    while(j<k && A.get(k) == A.get(k+1)) k--;
+                int sum = A.get(i) + A.get(j) + A.get(k);
+                int diff = Math.abs(sum - B);
+                if (diff == 0) {
+                    return B;
                 }
-                else if (A.get(j) + A.get(k) > target) k--;
-                else {
+                if (diff < min) {
+                    min = diff;
+                    res = sum;
+                }
+                if(sum <= B) {
                     j++;
+                }
+                else {
+                    k--;
                 }
             }
         }
+        return res;
         
-        return ans;
         
     }
 }
