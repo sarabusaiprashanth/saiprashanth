a0b2d15017ec916b2ef32f6dfef3e250b0fcfc9f
sarabusaiprashanth <49392113+sarabusaiprashanth@users.noreply.github.com>
2019-04-09 16:20:40 +0530

    Update braces.java

--- programs/braces.java
+++ programs/braces.java
@@ -2,9 +2,10 @@ public class Solution {
 	public int braces(String a) {
 	    Stack<Character> stack = new Stack<Character>();
 	    for(int i = 0; i < a.length(); i++) {
-	        if(a.charAt(i) == '(' || a.charAt(i) == '+' || a.charAt(i) == '-' || a.charAt(i) == '*' || a.charAt(i) == '/') {
+	        if(a.charAt(i) == '/' || a.charAt(i) == '*' || a.charAt(i) == '-' || a.charAt(i) == '+' || a.charAt(i) == ')') {
 	            stack.push(a.charAt(i));
-	        } else if (a.charAt(i) == ')') 
+	        }
+		else if (a.charAt(i) == ')') 
 	        {
 	            int flag = 0;
 	             while(stack.peek() != '(')
